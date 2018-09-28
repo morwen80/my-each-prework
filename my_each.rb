a935dba0dc375c749f99ab9ca69595b9bc938dc2
Valeria Ragonese <ribesnero80@gmail.com>
2018-09-28 17:47:56 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+# def my_each(arr)
+#   i = 0;
+#   while i < arr.length { |word| word}
+#   i +=1 
+# end
+# yield myeach 
+# end
+
+def my_each(arr)
+  yield
 end
+i = 0 
+while i < arr.length {|word| word}
+i += 1 
+end
+end 
